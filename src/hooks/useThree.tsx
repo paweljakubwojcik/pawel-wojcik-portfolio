e8ef27fe2cfe9fe1a489e98f68f63d0868504558
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-07-13 23:41:39 +0200

    fixed issue #1 - streched moon

--- src/hooks/useThree.tsx
+++ src/hooks/useThree.tsx
@@ -1,31 +1,41 @@
 import React, { useMemo, useCallback, useState, useEffect } from 'react'
 import { Scene, PerspectiveCamera, WebGLRenderer, Clock, TextureLoader } from 'three'
 
+const isBrowser = typeof window !== `undefined`
+
 const fov = 50
-const aspectRatio = typeof window !== `undefined` ? window.innerWidth / window.innerHeight : undefined
+const aspectRatio = isBrowser ? window.innerWidth / window.innerHeight : undefined
 const near = 0.1
 const far = 10e4
 
 export default function useThree() {
-  const scene = useMemo(() => (typeof window !== `undefined` ? new Scene() : null), [])
-  const camera = useMemo(
-    () => (typeof window !== `undefined` ? new PerspectiveCamera(fov, aspectRatio, near, far) : null),
-    []
-  )
-  const renderer = useMemo(() => (typeof window !== `undefined` ? new WebGLRenderer({ alpha: true }) : null), [])
-  const clock = useMemo(() => (typeof window !== `undefined` ? new Clock() : null), [])
+  const scene = useMemo(() => (isBrowser ? new Scene() : null), [])
+  const camera = useMemo(() => (isBrowser ? new PerspectiveCamera(fov, aspectRatio, near, far) : null), [])
+  const renderer = useMemo(() => (isBrowser ? new WebGLRenderer({ alpha: true }) : null), [])
+  const clock = useMemo(() => (isBrowser ? new Clock() : null), [])
   const textureLoader = useMemo(() => new TextureLoader(), [])
 
   const render = useCallback(() => {
     requestAnimationFrame(() => renderer.render(scene, camera))
   }, [scene, camera, renderer])
 
-  const resize = useCallback(() => {
+  const setCanvasSize = () => {
     camera.aspect = window?.innerWidth / window?.innerHeight
     renderer.setSize(window?.innerWidth, window?.innerHeight)
     camera.updateProjectionMatrix()
+  }
+
+  const resize = useCallback(() => {
+    setCanvasSize()
     render()
   }, [camera, renderer, render])
+
+  //on Mount
+  useEffect(() => {
+    if (isBrowser) {
+      setCanvasSize()
+    }
+  }, [])
 
   useEffect(() => {
     window.addEventListener('resize', resize)
