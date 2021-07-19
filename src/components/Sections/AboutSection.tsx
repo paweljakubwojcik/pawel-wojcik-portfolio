ad78ad5e890a75c2ed58458578de006b84ba873f
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-07-19 15:26:30 +0200

    made About Section content to be fetched from CMS

--- src/components/Sections/AboutSection.tsx
+++ src/components/Sections/AboutSection.tsx
@@ -5,18 +5,27 @@ import MediaQuery from '../MediaQuery'
 import { useTheme } from 'styled-components'
 import { PropsFromSnapscrollSection } from '../../typescript'
 import { ReactBaseProps } from 'react-markdown/src/ast-to-react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 export default function AboutSection({ visible, wholeView, ...rest }: PropsFromSnapscrollSection & ReactBaseProps) {
   const { breakpoints } = useTheme()
+
+  const {
+    graphCmsContent: { content },
+  } = useStaticQuery(graphql`
+    query AboutDescription {
+      graphCmsContent(name: { eq: "AboutSectionContent" }) {
+        name
+        content
+      }
+    }
+  `)
+
   return (
     <Section {...rest}>
       <Section.Column>
         <Section.Title>About Me</Section.Title>
-        <Section.Paragraph>
-          Hello there, I’m a junior front-end developer, and student on Warsaw University of Technology. I’ve been
-          intensively learning web technologies for the past year. Right now, I’m looking for opportunities to learn new
-          and to develop existing skills, by building ambitious project with experienced teams.
-        </Section.Paragraph>
+        <Section.Paragraph>{content}</Section.Paragraph>
       </Section.Column>
       <MediaQuery query={`(min-width: ${breakpoints.MIN_LAPTOP}px)`}>
         <Section.Column>
